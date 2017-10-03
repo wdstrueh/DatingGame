eef36a01def79f043db93452cb7f579a81bce7f0
wdstrueh <31040286+wdstrueh@users.noreply.github.com>
2017-10-03 08:28:20 -0500

    Update Year3000.java

--- Year3000.java
+++ Year3000.java
@@ -1,3 +1,4 @@
+package datingGame;
 import java.io.*;
 
 /**
@@ -6,17 +7,26 @@ import java.io.*;
 */
 public class Year3000
 {
-  public static void main(String [] args) throws IOException
-  {
-    DateInterface d = new MyDate();
-    d.set(1,25,2015,0);  // sets the date to Sunday, January 25th, 2015
-    while(d.getYear()<3000)
-    {
-      d.tomorrow();
-      // un-comment the next line to help with debugging
-      // System.out.println(d);
-    }
-    // at this point, d represents January 1, 3000
-    System.out.println(d);
-  }
+	public static void main(String [] args) throws IOException, InterruptedException
+	{
+		DateInterface d = new MyDate();
+		d.set(1,25,2015,0);  // sets the date to Sunday, January 25th, 2015
+		while(d.getYear() < 3000)
+		{
+			d.tomorrow();
+			// un-comment the next line to help with debugging
+			//System.out.println(d);
+		}
+		System.out.println(d);
+		d.set(1, 25, 2015, 0); // sets date to Sunday, January, 25th, 2015
+		while(d.getYear() > 1799)
+		{
+		// at this point, d represents January 1, 1800	
+			d.yesterday();
+			//System.out.println(d);
+		}
+		// at this point, d represents January 1, 3000
+		d.tomorrow();
+		System.out.println(d);
+	}
 }
